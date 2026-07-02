392345ac9a313c7c9ba09f96a21c82ed44c15d96
Aiden Fong <afong413@gmail.com>
2026-07-02 15:46:31 -0700

    Use uv

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 #!/usr/local/bin/python3
 
+import numpy as np
 import pygame
-from pygame.locals import *
-
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from pygame.locals import *
 
-import numpy as np
 
 class SoftBody:
     def __init__(self, mass, pts, r, springs, stiff, damp, outline):
@@ -33,9 +32,14 @@ class SoftBody:
     def reflect(self, v, r, loss):
         r = self.normalize(r)
         return np.array([loss * (v[i] - 2 * np.dot(v, r) * r[i]) for i in range(3)])
-    
+
     def springForce(self, s):
-        return (self.distance(self.ptPos[self.springPts[s][0]], self.ptPos[self.springPts[s][1]]) - self.restLength[s]) * self.stiff + np.dot(self.normalize(self.directionVector(self.ptPos[self.springPts[s][0]], self.ptPos[self.springPts[s][1]])), self.velocity[self.springPts[s][1]] - self.velocity[self.springPts[s][0]]) * self.damp
+        return (
+            self.distance(self.ptPos[self.springPts[s][0]], self.ptPos[self.springPts[s][1]]) - self.restLength[s]
+        ) * self.stiff + np.dot(
+            self.normalize(self.directionVector(self.ptPos[self.springPts[s][0]], self.ptPos[self.springPts[s][1]])),
+            self.velocity[self.springPts[s][1]] - self.velocity[self.springPts[s][0]],
+        ) * self.damp
 
     def calculateForces(self):
         self.force = [[0, 0, 0] for point in self.ptPos]
@@ -62,7 +66,7 @@ class SoftBody:
             self.velocity[i][0] = round(self.velocity[i][0], 5)
             self.velocity[i][1] = round(self.velocity[i][1], 5)
             self.velocity[i][2] = round(self.velocity[i][2], 5)
-            
+
         for i in range(len(self.ptPos)):
             self.ptPos[i][0] += self.velocity[i][0]
             self.ptPos[i][1] += self.velocity[i][1]
@@ -71,8 +75,7 @@ class SoftBody:
             self.ptPos[i][1] = round(self.ptPos[i][1], 5)
             self.ptPos[i][2] = round(self.ptPos[i][2], 5)
 
-            
-    def wallCollide(self):        
+    def wallCollide(self):
         for i in range(len(self.ptPos)):
             if self.ptPos[i][1] > 50:
                 self.ptPos[i][1] = 50
@@ -108,7 +111,8 @@ class SoftBody:
 
     def directionVector(self, p1, p2):
         return [p2[i] - p1[i] for i in range(len(p1))]
-                        
+
+
 class Rect(SoftBody):
     def __init__(self, position, mass, size, division, radii, stiffness, damping):
         x0, y0, z0 = position[0], position[1], position[2]
@@ -117,80 +121,166 @@ class Rect(SoftBody):
         for x in range(width + 1):
             for y in range(height + 1):
                 for z in range(depth + 1):
-                    massPoints += [[x0 + x * division - width * division / 2, y0 + y * division - height * division / 2, z0 + z * division - depth * division / 2]]
+                    massPoints += [
+                        [
+                            x0 + x * division - width * division / 2,
+                            y0 + y * division - height * division / 2,
+                            z0 + z * division - depth * division / 2,
+                        ]
+                    ]
         springs = []
         for x in range(width):
             for y in range(height + 1):
                 for z in range(depth + 1):
-                    springs += [(self.flatten(x, y, z, width, height, depth), self.flatten(x + 1, y, z, width, height, depth))]
+                    springs += [
+                        (self.flatten(x, y, z, width, height, depth), self.flatten(x + 1, y, z, width, height, depth))
+                    ]
         for x in range(width + 1):
             for y in range(height):
                 for z in range(depth + 1):
-                    springs += [(self.flatten(x, y, z, width, height, depth), self.flatten(x, y + 1, z, width, height, depth))]
+                    springs += [
+                        (self.flatten(x, y, z, width, height, depth), self.flatten(x, y + 1, z, width, height, depth))
+                    ]
         for x in range(width + 1):
             for y in range(height + 1):
                 for z in range(depth):
-                    springs += [(self.flatten(x, y, z, width, height, depth), self.flatten(x, y, z + 1, width, height, depth))]
+                    springs += [
+                        (self.flatten(x, y, z, width, height, depth), self.flatten(x, y, z + 1, width, height, depth))
+                    ]
         for x in range(width):
             for y in range(height):
                 for z in range(depth):
-                    springs += [(self.flatten(x, y, z, width, height, depth), self.flatten(x + 1, y + 1, z + 1, width, height, depth))]
-                    springs += [(self.flatten(x + 1, y, z, width, height, depth), self.flatten(x, y + 1, z + 1, width, height, depth))]
-                    springs += [(self.flatten(x, y + 1, z, width, height, depth), self.flatten(x + 1, y, z + 1, width, height, depth))]
+                    springs += [
+                        (
+                            self.flatten(x, y, z, width, height, depth),
+                            self.flatten(x + 1, y + 1, z + 1, width, height, depth),
+                        )
+                    ]
+                    springs += [
+                        (
+                            self.flatten(x + 1, y, z, width, height, depth),
+                            self.flatten(x, y + 1, z + 1, width, height, depth),
+                        )
+                    ]
+                    springs += [
+                        (
+                            self.flatten(x, y + 1, z, width, height, depth),
+                            self.flatten(x + 1, y, z + 1, width, height, depth),
+                        )
+                    ]
         for x in range(width):
             for y in range(height):
                 for z in range(depth + 1):
-                    springs += [(self.flatten(x, y, z, width, height, depth), self.flatten(x + 1, y + 1, z, width, height, depth))]
-                    springs += [(self.flatten(x + 1, y, z, width, height, depth), self.flatten(x, y + 1, z, width, height, depth))]
+                    springs += [
+                        (
+                            self.flatten(x, y, z, width, height, depth),
+                            self.flatten(x + 1, y + 1, z, width, height, depth),
+                        )
+                    ]
+                    springs += [
+                        (
+                            self.flatten(x + 1, y, z, width, height, depth),
+                            self.flatten(x, y + 1, z, width, height, depth),
+                        )
+                    ]
         for x in range(width):
             for y in range(height + 1):
                 for z in range(depth):
-                    springs += [(self.flatten(x, y, z, width, height, depth), self.flatten(x + 1, y, z + 1, width, height, depth))]
-                    springs += [(self.flatten(x + 1, y, z, width, height, depth), self.flatten(x, y, z + 1, width, height, depth))]
+                    springs += [
+                        (
+                            self.flatten(x, y, z, width, height, depth),
+                            self.flatten(x + 1, y, z + 1, width, height, depth),
+                        )
+                    ]
+                    springs += [
+                        (
+                            self.flatten(x + 1, y, z, width, height, depth),
+                            self.flatten(x, y, z + 1, width, height, depth),
+                        )
+                    ]
         for x in range(width + 1):
             for y in range(height):
                 for z in range(depth):
-                    springs += [(self.flatten(x, y, z, width, height, depth), self.flatten(x, y + 1, z + 1, width, height, depth))]
-                    springs += [(self.flatten(x, y + 1, z, width, height, depth), self.flatten(x, y, z + 1, width, height, depth))]
+                    springs += [
+                        (
+                            self.flatten(x, y, z, width, height, depth),
+                            self.flatten(x, y + 1, z + 1, width, height, depth),
+                        )
+                    ]
+                    springs += [
+                        (
+                            self.flatten(x, y + 1, z, width, height, depth),
+                            self.flatten(x, y, z + 1, width, height, depth),
+                        )
+                    ]
         outline = []
         for x in range(width):
             outline += [(self.flatten(x, 0, 0, width, height, depth), self.flatten(x + 1, 0, 0, width, height, depth))]
-            outline += [(self.flatten(x, height, 0, width, height, depth), self.flatten(x + 1, height, 0, width, height, depth))]
-            outline += [(self.flatten(x, 0, depth, width, height, depth), self.flatten(x + 1, 0, depth, width, height, depth))]
-            outline += [(self.flatten(x, height, depth, width, height, depth), self.flatten(x + 1, height, depth, width, height, depth))]
+            outline += [
+                (self.flatten(x, height, 0, width, height, depth), self.flatten(x + 1, height, 0, width, height, depth))
+            ]
+            outline += [
+                (self.flatten(x, 0, depth, width, height, depth), self.flatten(x + 1, 0, depth, width, height, depth))
+            ]
+            outline += [
+                (
+                    self.flatten(x, height, depth, width, height, depth),
+                    self.flatten(x + 1, height, depth, width, height, depth),
+                )
+            ]
         for y in range(height):
             outline += [(self.flatten(0, y, 0, width, height, depth), self.flatten(0, y + 1, 0, width, height, depth))]
-            outline += [(self.flatten(width, y, 0, width, height, depth), self.flatten(width, y + 1, 0, width, height, depth))]
-            outline += [(self.flatten(0, y, depth, width, height, depth), self.flatten(0, y + 1, depth, width, height, depth))]
-            outline += [(self.flatten(width, y, depth, width, height, depth), self.flatten(width, y + 1, depth, width, height, depth))]
+            outline += [
+                (self.flatten(width, y, 0, width, height, depth), self.flatten(width, y + 1, 0, width, height, depth))
+            ]
+            outline += [
+                (self.flatten(0, y, depth, width, height, depth), self.flatten(0, y + 1, depth, width, height, depth))
+            ]
+            outline += [
+                (
+                    self.flatten(width, y, depth, width, height, depth),
+                    self.flatten(width, y + 1, depth, width, height, depth),
+                )
+            ]
         for z in range(depth):
             outline += [(self.flatten(0, 0, z, width, height, depth), self.flatten(0, 0, z + 1, width, height, depth))]
-            outline += [(self.flatten(width, 0, z, width, height, depth), self.flatten(width, 0, z + 1, width, height, depth))]
-            outline += [(self.flatten(0, height, z, width, height, depth), self.flatten(0, height, z + 1, width, height, depth))]
-            outline += [(self.flatten(width, height, z, width, height, depth), self.flatten(width, height, z + 1, width, height, depth))]
+            outline += [
+                (self.flatten(width, 0, z, width, height, depth), self.flatten(width, 0, z + 1, width, height, depth))
+            ]
+            outline += [
+                (self.flatten(0, height, z, width, height, depth), self.flatten(0, height, z + 1, width, height, depth))
+            ]
+            outline += [
+                (
+                    self.flatten(width, height, z, width, height, depth),
+                    self.flatten(width, height, z + 1, width, height, depth),
+                )
+            ]
         super().__init__(mass, massPoints, radii, springs, stiffness, damping, outline)
 
     def flatten(self, x, y, z, w, h, d):
         return x * (h + 1) * (d + 1) + y * (d + 1) + z
-    
+
+
 def drawFloor(width, depth, division):
     glBegin(GL_LINES)
-    glColor3fv((0,1,0))
+    glColor3fv((0, 1, 0))
     for x in range(width + 1):
         glVertex3fv((x * division - width * division / 2, -50, depth * division / 2))
-        glVertex3fv((x * division - width * division / 2, -50, - depth * division / 2))
+        glVertex3fv((x * division - width * division / 2, -50, -depth * division / 2))
     for z in range(depth + 1):
         glVertex3fv((width * division / 2, -50, z * division - depth * division / 2))
-        glVertex3fv(( - width * division / 2, -50, z * division - depth * division / 2))
+        glVertex3fv((-width * division / 2, -50, z * division - depth * division / 2))
     glEnd()
-    
+
+
 softBodies = []
 softBodies += [Rect((0, 0, 0), 1000, (3, 3, 3), 5, 1, 0.1, 0.05)]
 
 pygame.init()
-pygame.display.set_mode((1280, 720), DOUBLEBUF|OPENGL)
+pygame.display.set_mode((1280, 720), DOUBLEBUF | OPENGL)
 pygame.display.set_caption('Soft Body Simulator 2.1')
-gluPerspective(75, 1280/720, 0.1, 500)
+gluPerspective(75, 1280 / 720, 0.1, 500)
 gluLookAt(0, 30, -80, 0, -50, 0, 0, 1, 0)
 
 clock = pygame.time.Clock()
@@ -214,19 +304,31 @@ while running:
         softBodies[i].wallCollide()
         softBodies[i].selfCollide()
 
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glEnable(GL_DEPTH_TEST)
 
     drawFloor(12, 12, 10)
 
     for i in range(len(softBodies)):
         glBegin(GL_LINES)
-        glColor3fv((1,1,1))
+        glColor3fv((1, 1, 1))
         for spring in softBodies[i].outline:
-            glVertex3fv((softBodies[i].ptPos[spring[0]][0], softBodies[i].ptPos[spring[0]][1], softBodies[i].ptPos[spring[0]][2]))
-            glVertex3fv((softBodies[i].ptPos[spring[1]][0], softBodies[i].ptPos[spring[1]][1], softBodies[i].ptPos[spring[1]][2]))
+            glVertex3fv(
+                (
+                    softBodies[i].ptPos[spring[0]][0],
+                    softBodies[i].ptPos[spring[0]][1],
+                    softBodies[i].ptPos[spring[0]][2],
+                )
+            )
+            glVertex3fv(
+                (
+                    softBodies[i].ptPos[spring[1]][0],
+                    softBodies[i].ptPos[spring[1]][1],
+                    softBodies[i].ptPos[spring[1]][2],
+                )
+            )
         glEnd()
 
     pygame.display.flip()
-            
+
 pygame.quit()
